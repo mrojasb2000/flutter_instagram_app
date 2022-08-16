db652c70cc0a61e4ecc6d10710e623560eb009eb
Mauricio Rojas <mrojase@vitamina.cl>
2022-08-16 18:27:37 -0400

    feat: add login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_app/screens/login_screen.dart';
 import 'package:flutter_instagram_app/utils/colors.dart';
-import 'package:flutter_instagram_app/utils/mobile_screen_layout.dart';
-import 'package:flutter_instagram_app/utils/responsive_layout_screen.dart';
-import 'package:flutter_instagram_app/utils/web_screen_layout.dart';
+// import 'package:flutter_instagram_app/responsive/mobile_screen_layout.dart';
+// import 'package:flutter_instagram_app/responsive/responsive_layout_screen.dart';
+// import 'package:flutter_instagram_app/responsive/web_screen_layout.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyBCgCvVvrx28yirBHntYQZV2-TfE2TUG-U',
+        appId: '1:330693832608:web:be0b7758d39a09c0717ec5',
+        messagingSenderId: '330693832608',
+        projectId: 'instagram-tut-ef438',
+        storageBucket: 'instagram-tut-ef438.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -23,9 +38,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroudColor,
       ),
-      home: const ResponsiveLayout(
-          mobileScreenLayout: MobileScreenLayout(),
-          webScreenLayout: WebScreenLayout()),
+      // home: const ResponsiveLayout(
+      //     mobileScreenLayout: MobileScreenLayout(),
+      //     webScreenLayout: WebScreenLayout()),
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_app/utils/colors.dart';
+import 'package:flutter_instagram_app/widgets/text_field_input.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(flex: 2, child: Container()),
+              // svg image
+              SvgPicture.asset(
+                'assets/ic_instagram.svg',
+                color: primaryColor,
+                height: 64,
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              // text field input for email
+              TextFieldInput(
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              // text field input for password
+              TextFieldInput(
+                hintText: 'Enter your password',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPass: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              // button login
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration:
+                    const ShapeDecoration(shape: RoundedRectangleBorder()),
+                child: const Text('Log in'),
+              )
+              // Transitioning to singing up
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
